41f75cd5a1c6da9ce6560e640fe3b4771c5f1d04
Emedolly <animelaemeema@gmail.com>
2025-09-21 23:55:38 +0530

    Type inference and updated the notes

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let lname = 'Eme';
+let lname;
 // lname = 10; // gives error because lname is of type string and cannot be assigned a number. 
+lname = "Emeema";
+let newname = lname.toUpperCase();
+console.log(newname);
 //# sourceMappingURL=datatypes.js.map
--- datatypes.ts
+++ datatypes.ts
@@ -1,2 +1,5 @@
-let lname='Eme';
+let lname : string;
 // lname = 10; // gives error because lname is of type string and cannot be assigned a number. 
+lname = "Emeema";
+let newname= lname.toUpperCase();
+console.log(newname);

80535cfe6791cdb7c8fafad4ec7088e47578bbfb
Emedolly <animelaemeema@gmail.com>
2025-09-22 21:29:21 +0530

    Number,boolean,Array,Enum data types practise and the overview

--- datatypes.js
+++ datatypes.js
@@ -5,4 +5,36 @@ let lname;
 lname = "Emeema";
 let newname = lname.toUpperCase();
 console.log(newname);
+// number:
+let age;
+// age = "10"; // gives error because age is of type number and cannot be assigned a string. 
+age = 10.5;
+age = 10;
+let result = parseInt("10.5");
+console.log(result);
+console.log(age);
+//boolean 
+let isvalid = false;
+console.log(isvalid);
+//Array are of 2 ways both are same can use anyone
+let empList;
+empList = ["Emeema", "Anu", "Asha"];
+let numList; //generic array type
+numList = [10, 20, 30];
+let results = numList.filter(N => N >= 2);
+let results1 = numList.find((num) => num === 20);
+let sum = numList.reduce((accum, current) => accum + current); //this is useful when you have mch products to caluclate within an object
+console.log(empList);
+console.log(numList);
+console.log(results);
+console.log(results1);
+console.log(sum);
+//Enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Blue;
 //# sourceMappingURL=datatypes.js.map
--- datatypes.ts
+++ datatypes.ts
@@ -1,5 +1,43 @@
+
 let lname : string;
 // lname = 10; // gives error because lname is of type string and cannot be assigned a number. 
 lname = "Emeema";
 let newname= lname.toUpperCase();
 console.log(newname);
+
+// number:
+let age : number;
+// age = "10"; // gives error because age is of type number and cannot be assigned a string. 
+age = 10.5;
+age = 10;
+let result = parseInt("10.5");
+console.log(result);
+console.log(age);
+
+//boolean 
+
+let isvalid : boolean=false;
+console.log(isvalid);
+
+//Array are of 2 ways both are same can use anyone
+let empList :string[];
+empList = ["Emeema","Anu","Asha"];
+let numList:Array<number>;//generic array type
+numList = [10,20,30];
+let results = numList.filter(N => N>=2);
+let results1 = numList.find((num)=>num===20);
+let sum = numList.reduce((accum,current)=>accum+current); //this is useful when you have mch products to caluclate within an object
+console.log(empList);
+console.log(numList);
+console.log(results);
+console.log(results1);
+console.log(sum);
+
+//Enum
+const enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+let c: Color = Color.Blue;
